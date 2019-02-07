ea3d690d82b24199c318ad27a2deb1377f793615
sdim-ua <ingersol@yandex.ua>
2019-02-07 22:24:11 +0400

    Create Solution.java

---
+++ Solution.java
@@ -0,0 +1,23 @@
+
+import java.util.ArrayList;
+
+/* 
+Что за список такой?
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        //напишите тут ваш код
+        ArrayList<String> list = new ArrayList<String>();
+        list.add("aaaaaa");
+        list.add("bbbbbb");
+        list.add("cccccc"); 
+        list.add("dddddd");
+        list.add("eeeeee");
+        System.out.println(list.size());
+        
+        for (int i = 0; int < list.size(); int++ ){
+            System.out.println(list.get(i))
+        }
+    }
+}
